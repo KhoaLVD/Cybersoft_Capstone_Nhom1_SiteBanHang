602d5996991ea7a85f30df8c2bd4e5671a034cb3
HyhTonHappy <tanphuchuynh2547@gmail.com>
2024-10-19 08:36:47 +0700

    update

---
+++ src/models/carts.js
@@ -0,0 +1,11 @@
+class Cart{
+    constructor(_id, _name, _price, _img, _type, _quantity){
+        this.id = _id;
+        this.name = _name;
+        this.price = _price;
+        this.img = _img;
+        this.type = _type;
+        this.quantity = _quantity;
+    }
+}
+export default Cart;
